de5f71173106219af0576e026175fa79658b229e
Airat <khasanofficial14@gmail.com>
2021-05-06 17:01:03 +0300

    add scroll for todolist and validate data

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import {Navbar} from './src/Navbar'
 import {AddTodo} from './src/AddTodo'
 import {Todo} from './src/Todo'
@@ -9,10 +9,12 @@ export default function App() {
   const [todos, setTodos] = useState([])
 
   const addTodo = (title) => {
+
     const newTodo = {
       id: Date.now().toString(),
       title
     }
+
     setTodos(prev => [
       ...prev,
       {
@@ -20,16 +22,22 @@ export default function App() {
       title
       }
     ])
-}
+  }
  
   return (
     <View>
       <Navbar title='Todo App'/>
       <View style={styles.container}>
         <AddTodo onSubmit={addTodo}/>
-        <View>
-          { todos.map(todo => {return <Todo todo={todo} key={todo.id} />})}
-        </View>
+
+        <FlatList
+        keyExtractor={(item => item.id.toString())}
+          data={todos}
+          renderItem={({item}) => (
+            <Todo todo={item} /> 
+            ) }
+        />
+
       </View>
     </View>
   );
--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,14 +1,27 @@
-import React from 'react'
-import {View, StyleSheet, TextInput, Button} from 'react-native'
+import React, {useState} from 'react'
+import {View, StyleSheet, TextInput, Button, Alert} from 'react-native'
 
 export const AddTodo = ({onSubmit }) => {
+    const [value, setValue] = useState('')
 
     const pressHandler = () => {
-        onSubmit('test todo')
+        if (value.trim()) {
+            onSubmit(value)
+            setValue('')
+        } else {
+            Alert.alert("Название дела не должно быть пустым")
+        }
     }
     return (
         <View style={styles.block}>
-            <TextInput style={styles.input}/>
+            <TextInput 
+            style={styles.input}
+            onChangeText={setValue} 
+            value={value}
+            placeholder="Введите название дела..."
+            autoCorrect={false}
+            autoCapitalize='none'
+            />
             <Button title="Добавить" onPress={pressHandler} />
         </View>
     )
