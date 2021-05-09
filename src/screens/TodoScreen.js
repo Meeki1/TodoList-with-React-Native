e0dc1a4027595b44192f8ae0ccca011f03cb9c03
Airat <khasanofficial14@gmail.com>
2021-05-09 20:25:54 +0300

    add Context for TodoScreen and MainScreen

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,48 +1,56 @@
-import React, {useState} from 'react'
-import { StyleSheet, View, Dimensions } from 'react-native'
-import {FontAwesome, AntDesign} from '@expo/vector-icons'
-import { EditModel } from '../components/EditModel'
-import { AppCard } from '../components/ui/AppCard'
-import { THEME } from '../theme'
-import {AppTextBold} from '../components/ui/AppTextBold'
-import {AppButton} from '../components/ui/AppButton'
+import React, { useState, useContext } from "react"
+import { StyleSheet, View, Dimensions } from "react-native"
+import { FontAwesome, AntDesign } from "@expo/vector-icons"
+import { EditModel } from "../components/EditModel"
+import { AppCard } from "../components/ui/AppCard"
+import { THEME } from "../theme"
+import { AppTextBold } from "../components/ui/AppTextBold"
+import { AppButton } from "../components/ui/AppButton"
+import { TodoContext } from "../context/todo/todoContext"
+import { ScreenContext } from "../context/screen/screenContext"
 
-export const TodoScreen = ({ goBack, todo, onRemove, onSave }) => {
+export const TodoScreen = () => {
+  const { todos, updateTodo, removeTodo } = useContext(TodoContext)
+  const { todoId, changeScreen } = useContext(ScreenContext)
   const [model, setModel] = useState(false)
 
-    const saveHandler = title => {
-      onSave(todo.id, title)
-      setModel(false)
-    }
+  const todo = todos.find((t) => t.id === todoId)
+
+  const saveHandler = (title) => {
+    updateTodo(todo.id, title)
+    setModel(false)
+  }
 
   return (
     <View>
-      <EditModel 
-        value={todo.title} 
-        visible={model} 
-        onCalcel={() => setModel(false)} 
+      <EditModel
+        value={todo.title}
+        visible={model}
+        onCalcel={() => setModel(false)}
         onSave={saveHandler}
-        />
-      <AppCard style={styles.card} >
+      />
+      <AppCard style={styles.card}>
         <AppTextBold style={styles.title}> {todo.title} </AppTextBold>
         <AppButton onPress={() => setModel(true)}>
-          <FontAwesome name='edit' size={20}/>
+          <FontAwesome name="edit" size={20} />
         </AppButton>
       </AppCard>
 
       <View style={styles.buttons}>
         <View style={styles.button}>
-          <AppButton onPress={goBack} color={THEME.GREY_COLOR}>
-            <AntDesign name='back' size={20} color='#fff'/>
+          <AppButton
+            onPress={() => changeScreen(null)}
+            color={THEME.GREY_COLOR}
+          >
+            <AntDesign name="back" size={20} color="#fff" />
           </AppButton>
         </View>
         <View style={styles.button}>
           <AppButton
             color={THEME.DANGER_COLOR}
-            onPress={() => onRemove(todo.id)}
-            >
-            
-            <FontAwesome name='remove' size='20' color='#fff' />
+            onPress={() => removeTodo(todo.id)}
+          >
+            <FontAwesome name="remove" size={20} color="#fff" />
           </AppButton>
         </View>
       </View>
@@ -51,19 +59,19 @@ export const TodoScreen = ({ goBack, todo, onRemove, onSave }) => {
 }
 
 const styles = StyleSheet.create({
-    buttons: {
-        flexDirection: 'row',
-        justifyContent: 'space-between'
-    },
-    button: {
-        // width: Dimensions.get('window').width
-        width: Dimensions.get('window').width > 400 ? 150 : 100
-    },
-    title: {
-        fontSize: 20
-    },
-    card: {
-        marginBottom: 20,
-        padding: 15
-    }
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  button: {
+    // width: Dimensions.get('window').width
+    width: Dimensions.get("window").width > 400 ? 150 : 100,
+  },
+  title: {
+    fontSize: 20,
+  },
+  card: {
+    marginBottom: 20,
+    padding: 15,
+  },
 })
